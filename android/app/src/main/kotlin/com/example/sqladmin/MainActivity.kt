3cb516613f39c208141ae19f668e8d21adee35e2
在菲欧坦的牛马 <1961526966@qq.com>
2025-01-07 18:14:54 +0800

    first commit

---
+++ android/app/src/main/kotlin/com/example/sqladmin/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.sqladmin
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,52 @@
+import 'package:english_words/english_words.dart';
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return ChangeNotifierProvider(
+      create: (context) => MyAppState(),
+      child: MaterialApp(
+        title: 'Namer App',
+        theme: ThemeData(
+          useMaterial3: true,
+          colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepOrange),
+        ),
+        home: MyHomePage(),
+      ),
+    );
+  }
+}
+
+class MyAppState extends ChangeNotifier {
+  var current = WordPair.random();
+}
+
+class MyHomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    var appState = context.watch<MyAppState>();
+
+    return Scaffold(
+      body: Column(
+        children: [
+          Text('原神'),
+          Text("世界上最好的游戏"),
+          ElevatedButton(
+            onPressed: () {
+              print('启动按钮触发');
+            },
+            child: Text("启动"),
+          )
+        ],
+      ),
+    );
+  }
+}
